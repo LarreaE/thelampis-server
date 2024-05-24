7a13afbda9b3b785220e0097bbb4d9a974db04f6
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-05-24 16:42:45 +0100

    fixing cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json()); // Middleware para parsear el cuerpo de las solicitudes
 app.use(bodyParser.urlencoded({ extended: false })); // Middleware para parsear el cuerpo de las solicitudes URL-encoded
 
 // Middleware para permitir solicitudes CORS
-app.use(function(req, res, next) {
+app.options(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
